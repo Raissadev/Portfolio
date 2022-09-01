4722d1303660ee09d0f0098b5e7314d4d2601886
Raissadev <raissa.fullstack@gmail.com>
2022-09-01 10:35:31 -0300

    ...

--- Frontend/src/components/Home/BannerComponent.tsx
+++ Frontend/src/components/Home/BannerComponent.tsx
@@ -1,4 +1,5 @@
-import { Row, Layout, Image, Typography } from 'antd';
+import { Row, Col, Layout, Image, Typography, Button } from 'antd';
+import { RightOutlined, CaretLeftFilled } from '@ant-design/icons';
 import myImage from '../../assets/my.jpg';
 
 const { Title } = Typography;
@@ -12,8 +13,22 @@ function BannerComponent(): any
                     <Image width={300} src={myImage} />
                 </Row>
                 <Row>
-                    <Title level={1}>
-                        INDEPENDENT FULL-STACK <br /> SOFTWARE ENGINNER
+                    <Col span={24}>
+                        <Title level={1}>
+                            INDEPENDENT FULL-STACK <br /> SOFTWARE ENGINNER
+                        </Title>
+                    </Col>
+                    <Col span={24}>
+                        <Button type="primary" size="large">
+                            Send mail
+                            <RightOutlined />
+                        </Button>
+                    </Col>
+                </Row>
+                <Row className="attach">
+                    <Title level={4}>
+                        <span>--------------- <CaretLeftFilled /></span>
+                        SCROLL TO SEE MORE
                     </Title>
                 </Row>
             </Layout>
--- Frontend/src/views/Home.tsx
+++ Frontend/src/views/Home.tsx
@@ -1,14 +1,20 @@
 import { Layout } from 'antd';
 import Head from "../components/Header";
 import BannerComponent from '../components/Home/BannerComponent';
+import MessageComponent from '../components/Home/MessageComponent';
 
 function Home(): any
 {
     return(
         <>
             <Head />
+
             <Layout>
                 <BannerComponent />
+            </Layout>
+
+            <Layout>
+                <MessageComponent />
             </Layout>
         </>
     );
